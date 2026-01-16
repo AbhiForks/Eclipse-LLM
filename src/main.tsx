09674974f505508b29db27d4d777182168478cb9
Abhilash <abhilashvishwa12@gmail.com>
2026-01-16 09:13:42 +0530

    docs: add documentation to application entry point
    - Add module-level JSDoc documentation explaining entry point
    - Document React root creation and rendering process
    - Add clear comments for initialization steps

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,15 @@
+/**
+ * main.tsx
+ *
+ * Application entry point.
+ * Initializes the React application and mounts it to the DOM.
+ */
 
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-// Create a root and render the app
+/**
+ * Create a root and render the application
+ */
 createRoot(document.getElementById("root")!).render(<App />);
